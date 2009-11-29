f753b7e253a55e462a6281d34ccc68403d4bbb30
Masahiro Nagano <kazeburo@ubuntu.localdomain>
2009-11-28 21:39:43 -0800

    initial commit

---
+++ lib/Plack/Server/AnyEvent/Prefork.pm
@@ -0,0 +1,33 @@
+package Plack::Server::AnyEvent::Prefork;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Server::AnyEvent::Prefork -
+
+=head1 SYNOPSIS
+
+  use Plack::Server::AnyEvent::Prefork;
+
+=head1 DESCRIPTION
+
+Plack::Server::AnyEvent::Prefork is
+
+=head1 AUTHOR
+
+kazeburo E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
